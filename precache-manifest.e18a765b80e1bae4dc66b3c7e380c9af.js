661748895e923df2f18340db2a5cc453a8620e1c
akash yadav <akashyadavmrt24@gmail.com>
2021-05-07 21:44:03 +0530

    Updates

---
+++ precache-manifest.e18a765b80e1bae4dc66b3c7e380c9af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b2557a8e5a76559b1f649e21394a8e",
+    "url": "/city-tour-/index.html"
+  },
+  {
+    "revision": "b6612ad2e8df72cd4fdb",
+    "url": "/city-tour-/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "27d5b6d7042fa685d2e5",
+    "url": "/city-tour-/static/js/2.d41edc64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/city-tour-/static/js/2.d41edc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6612ad2e8df72cd4fdb",
+    "url": "/city-tour-/static/js/main.f3c19560.chunk.js"
+  },
+  {
+    "revision": "2715993160989e8afd67",
+    "url": "/city-tour-/static/js/runtime-main.56165c7f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/city-tour-/static/media/logo.ee7cd8ed.svg"
+  }
+]);
